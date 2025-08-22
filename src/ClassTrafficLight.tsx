d13003af93e34a9a8c01a444a235714e11e9b13a
Joshua Tryon <joshuamtryon@gmail.com>
2025-08-22 14:05:46 -0500

    solved

--- src/ClassTrafficLight.tsx
+++ src/ClassTrafficLight.tsx
@@ -1,17 +1,39 @@
 import { Component } from "react";
 
 export class ClassTrafficLight extends Component {
+  state = {
+    activeLight: 0,
+  };
+  nextLight = () => {
+    this.setState({
+      activeLight: this.state.activeLight < 2 ? this.state.activeLight + 1 : 0,
+    });
+  };
   render() {
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
           {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div
+            className={`circle ${
+              this.state.activeLight === 2 ? "red" : "black"
+            }`}
+          ></div>
+          <div
+            className={`circle ${
+              this.state.activeLight === 1 ? "yellow" : "black"
+            }`}
+          ></div>
+          <div
+            className={`circle ${
+              this.state.activeLight === 0 ? "green" : "black"
+            }`}
+          ></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.nextLight}>
+          Next State
+        </button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
+
 export const FunctionalTrafficLight = () => {
+  const [activeLight, setActiveLight] = useState(0);
+  const nextLight = () => {
+    setActiveLight(activeLight < 2 ? activeLight + 1 : 0);
+  };
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${activeLight === 2 ? "red" : "black"}`}></div>
+        <div
+          className={`circle ${activeLight === 1 ? "yellow" : "black"}`}
+        ></div>
+        <div
+          className={`circle ${activeLight === 0 ? "green" : "black"}`}
+        ></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={nextLight}>
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
